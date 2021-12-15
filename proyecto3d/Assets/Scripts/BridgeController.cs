32ea1ed37711dcced7002466e3b2bebfcd44e240
Alejandro Hernández <hernandezlara180@gmail.com>
2021-12-14 21:13:58 -0600

    corrección comp

--- proyecto3d/Assets/Scripts/BridgeController.cs
+++ proyecto3d/Assets/Scripts/BridgeController.cs
@@ -2,7 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.EventSystems;
+using UnityEngine.Events;
 
 
 public class BridgeController : MonoBehaviour
--- proyecto3d/Assets/Scripts/HealthController.cs
+++ proyecto3d/Assets/Scripts/HealthController.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System;
-using UnityEngine.EventSystems;
+using UnityEngine.Events;
 
 public class HealthController : MonoBehaviour
 {
